a18d3016d306bfa7b2d048996f1412e234167326
Vansh <vanshsureshsachdeva@gmail.com>
2026-07-05 20:44:59 +0530

    Complete Product CRUD API with MongoDB

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,10 +5,19 @@ const router = express.Router();
 const {
   createProduct,
   getProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
 } = require("../controllers/productController");
 
 router.post("/", createProduct);
 
 router.get("/", getProducts);
+
+router.get("/:id", getProductById);
+
+router.put("/:id", updateProduct);
+
+router.delete("/:id", deleteProduct);
 
 module.exports = router;
