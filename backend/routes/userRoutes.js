d1e78e54da2b83bccc6158498a5e96dfca5aa271
Vansh <vanshsureshsachdeva@gmail.com>
2026-07-09 21:06:16 +0530

    Add admin user management APIs

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,17 +7,29 @@ const {
   loginUser,
   getUserProfile,
   updateUserProfile,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
 } = require("../controllers/userController");
 
-const { protect } = require("../middleware/authMiddleware");
+const { protect, admin } = require("../middleware/authMiddleware");
 
 router.post("/register", registerUser);
 
 router.post("/login", loginUser);
 
+router.get("/", protect, admin, getUsers);
+
 router
   .route("/profile")
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
+
+router
+  .route("/:id")
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
+  .delete(protect, admin, deleteUser);
 
 module.exports = router;
